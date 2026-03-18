9e4d7edae9fe7d0ad9a70a49cf700e00fccf35a8
goloins <dsides.personal@gmail.com>
2026-03-18 10:53:52 -0500

    settings.php: Added switch statement for resource_type, documented as much.

--- settings.php
+++ settings.php
@@ -53,7 +53,7 @@ $ps_mysqli_settings = array(
             * 'HTML' - For scraping news items directly from HTML pages. As a wise man once said, you cannot use REGEX to parse HTML
                     However, we're stupid so we're going to do it anyway. Read on for more details on how to use this resource type.
             * 'API' - For scraping news items from APIs. Read on for more details on how to use this resource type.
-    * 'resource_settings' - An array that contains settings specific to the resource type being scraped. This points to an object
+    * 'resource_settings' - An array that contains settings specific to the resource type being scraped. This points to an array
                     customized for the resource type being scraped. 
 */
 
@@ -67,9 +67,50 @@ $ps = array(
     'resource_settings' => NULL         // Do not touch, this will be set later by internal logic.
     );
 
+$resource_settings_rss = array(
+    'feed_url' => 'https://www.myEXAMPLEfeed.com/rss/rss.php?id=1019', // The URL of the RSS feed to scrape. You can change this to any RSS feed you want.
+    'item_limit' => 10, // The maximum number of items to pull down
+);
+
+$resource_settings_html = array(
+    'target_url' => 'https://www.myEXAMPLEfeed.com/sections/technology/', // The URL of the HTML page to scrape. You can change this to any HTML page you want.
+    'item_selector' => '.item-info', // The CSS selector for the news items on the HTML page. This is used to identify the news items to scrape. You will need to inspect the HTML structure of the target page and update this selector accordingly.
+    'title_selector' => '.title', // The CSS selector for the title of the news item. This is used to extract the title of the news item from the HTML. You will need to inspect the HTML structure of the target page and update this selector accordingly.
+    'link_selector' => '.title a', // The CSS selector for the link of the news item. This is used to extract the link of the news item from the HTML. You will need to inspect the HTML structure of the target page and update this selector accordingly.
+    'description_selector' => '.teaser', // The CSS selector for the description of the news item. This is used to extract the description of the news item from the HTML. You will need to inspect the HTML structure of the target page and update this selector accordingly.
+);
+
+$resource_settings_api = array(
+    'endpoint_url' => 'https://api.myEXAMPLEfeed.com/query?fields=title,teaser,link&searchType=fullText&format=json&id=1019', // The URL of the API endpoint to scrape. You can change this to any API endpoint you want.
+    'item_limit' => 10, // The maximum number of items to pull down
+);
 
 
 
+/******************************************************************************************************/
+/* NO MORE EDIT LINE, YOU SHOULD NOT NEED TO EDIT BELOW THIS LINE UNLESS YOU KNOW WHAT YOU'RE DOING */
+/* CUSTOM FUNCTIONS AND METHODS SHOULD BE PLACED IN FUNCTIONS.PHP, NOT HERE. KTHXBAI <3 */
+/******************************************************************************************************/
+
+// Here we're going to select which array to point 'resource_settings' to based on the resource type.
+// If you're using a custom type, be sure you add it to the switch statement and point it to the appropriate settings array.
+// This is one of two places you'll need to add it, but that will be documented at some point. 
+// It's 11am on a Wednesday and I'm tired, cut me some slack.
+
+
+switch($ps['resource_type']) {
+    case 'RSS':
+        $ps['resource_settings'] = $resource_settings_rss;
+        break;
+    case 'HTML':
+        $ps['resource_settings'] = $resource_settings_html;
+        break;
+    case 'API':
+        $ps['resource_settings'] = $resource_settings_api;
+        break;
+    default:
+        die('Invalid resource type specified in $ps. You supplied: ' . $ps['resource_type'] . '. Valid options are: RSS, HTML, API. Typo?');
+}
 
 
 // Building the database connection if caching to the database is enabled, 
