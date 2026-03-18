8292dc7cb87f33eb0b05ca70be88988c24ea1fd6
dakota <dakota@206-it-lin.banks.inc>
2026-03-18 10:28:04 -0500

    Initial commit

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * functions.php
+ * @goloins 2026-03-18
+ * 
+ * Functions and Basic Methods for PocketSpigot
+ * Feel free to customize this as needed for your PocketSpigot instance. 
+ * This file contains all the necessary functions and basic methods that are used throughout the PocketSpigot project.
+ * 
+ * Part of the PocketSpigot Project
+ * Copyleft (c) 2026 PocketSpigot Team
+ * 
+ * This program is licensed under the Mozila Public License Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ */
+ 
+include_once 'settings.php';
+
